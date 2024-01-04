0fb8aa123e7b0fead6282bdefcbdceda6a3fd4e7
ic2dent <shamyratbabanazarow@gmail.com>
2024-01-05 01:49:30 +0500

    editing

--- advent_naughty/urls.py
+++ advent_naughty/urls.py
@@ -1,22 +1,18 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.static import serve
+from django.urls import path, include
 
 from main import views
 
 urlpatterns = [
-    path('djrichtextfield/', include('djrichtextfield.urls')),
-    path('advent/<int:user_id>/', views.advent, name='advent'),
-    path('send-message/', views.send_message, name='send_message'),
-] + [re_path(r"^static/(?P<path>.*)$", serve), re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
-
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {
-            'document_root': settings.MEDIA_ROOT,
-        }),
-    ]
+                  path('djrichtextfield/', include('djrichtextfield.urls')),
+                  path('advent/<int:user_id>/', views.advent, name='advent'),
+                  path('send-message/', views.send_message, name='send_message'),
+              ] + [
+                  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+                  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+              ]
 
 urlpatterns += [
     path('', admin.site.urls),
